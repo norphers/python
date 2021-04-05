607274263720fce0c10dca9e5c54321cca468925
Oriol Canadés <oriol.canades@in2.es>
2021-04-05 18:46:39 +0200

    exercies 1, 2, and 3

---
+++ first-steps/ex1.py
@@ -0,0 +1,11 @@
+# exxercise 1 - Hello World!
+# print() and running python file
+
+print("Hello world!")
+
+# to execute a python file, write command 'python nameFile.py'
+
+# exrecise 2
+# comments
+
+# This is a comment. As you can see, comments are preceded by a hash.
---
+++ first-steps/ex3.py
@@ -0,0 +1,14 @@
+# exercise3
+# numbers and maths
+
+print("Addition 3+4=", 3 + 4)
+print("Substraction 3-4 =", 3 - 4)
+print("Multiplication 3·4 =", 3 * 4)
+print("Division 3/4=", 3 / 4)
+print("Modulus (%) 7/3=", 7 % 3) # el resto de 7/3 es 1. Esto es lo que calcula el modulus.
+ 
+print(3 + 2 > 5 - 7) # boolean expression
+
+# order of operation
+
+# PEMDAS => Parentheses-Exponents-Multiplication-Division-Addition-Substraction
